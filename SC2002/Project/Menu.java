5ca3a2d47d41233fd806bb7354cb16b61346423d
maejikal <meylinggga@gmail.com>
2025-03-26 14:07:02 +0800

    update Menu.java
    took out manager menu

--- SC2002/Project/Menu.java
+++ SC2002/Project/Menu.java
@@ -10,6 +10,7 @@ public class Menu {
     private List<String> role;
     private List<String> applicantMenu;
     private List<String> enquiryMenu;
+    private List<String> managerMenu;
 
     // Constructor that uses a default welcome menu
     public Menu() {
@@ -43,6 +44,23 @@ public class Menu {
                 "8. Apply to become an officer",
                 "9. Log out and return to main program",
                 "Enter your choice: ");
+        this.managerMenu = Arrays.asList(
+            // System.out.println("--- Manager Menu ---");
+                "1. Create a Project",
+                "2. Edit a Project",
+                "3. Delete a Project",
+                "4. View All Projects",
+                "5. View My Projects",
+                "6. View Officer Registrations",
+                "7. Handle Officer Registration",
+                "8. Handle BTO Applications",
+                "9. Handle Withdrawal Requests",
+                "10. Generate Applicant Report",
+                "11. View All Enquiries",
+                "12. Handle Project Enquiries",
+                "13. View account details",
+                "14. Log out",
+                "Enter your choice: ");
         this.enquiryMenu = Arrays.asList(
                 "Please choose an option:",
                 "1. Make enquiry",
@@ -54,42 +72,40 @@ public class Menu {
     }
 
     // Optional: Constructor to allow passing a custom welcome menu
-    public Menu(List<String> welcomeMenu, List<String> loginError, List<String> role, List<String> applicantMenu, List<String> enquiryMenu) {
+    public Menu(List<String> welcomeMenu, List<String> loginError, List<String> role, List<String> applicantMenu, List<String> enquiryMenu, List<String> managerMenu) {
         this.welcomeMenu = welcomeMenu;
         this.loginError = loginError;
         this.role = role;
         this.applicantMenu = applicantMenu;
         this.enquiryMenu= enquiryMenu;
+        this.managerMenu= managerMenu;
 
+    }
+
+    public void printMenu(List<String> list) {
+        for (String option : list) {
+            System.out.println(option);
+        }
     }
 
     public void printWelcomeMenu() {
-        for (String menuItem : welcomeMenu) {
-            System.out.println(menuItem);
-        }
+        printMenu(welcomeMenu);
     }
 
     public void printloginError() {
-        for (String loginErrorItem : loginError) {
-            System.out.println(loginErrorItem);
-        }
+        printMenu(loginError);
     }
 
     public void printSelectRole() {
-        for (String selectRole : role) {
-            System.out.println(selectRole);
-        }
+        printMenu(role);
     }
-
     public void printApplicantMenu() {
-        for (String option : applicantMenu) {
-            System.out.println(option);
-        }
+        printMenu(applicantMenu);
     }
-
+    public void printManagerMenu() {
+        printMenu(managerMenu);
+    }
     public void printEnquiryMenu() {
-        for (String option : enquiryMenu) {
-            System.out.println(option);
-        }
+        printMenu(enquiryMenu);
     }
 }
